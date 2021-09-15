9c9698413b60d070f669094ec4385d04ac0f68d2
jakdev047 <jakfreelancer047@gmail.com>
2021-09-15 18:39:38 +0600

    add todo

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,21 +1,55 @@
 <template>
     <div class="add-todo-page">
-        <div class="container">
+        <div class="container my-5">
             <h3>Add Todo</h3>
+            <div class="add">
+               <form @submit="onSubmit">
+                   <input type="text" v-model="title" placeholder="Add Todo..."/>
+                   <input type="submit" value="Submit"/>
+                </form> 
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import {mapActions} from 'vuex';
     export default {
         name: 'add-todo-page',
         components: {},
-        data:()=>({}),
+        data:()=>({
+            title: ''
+        }),
         computed: {},
-        methods:{},
+        methods:{
+            ...mapActions({
+                addTodo: 'addTodo'
+            }),
+            onSubmit(e) {
+                e.preventDefault();
+                this.addTodo(this.title);
+            }
+        },
         async created () {},
         mounted () {}, 
     }
 </script>
 
-<style scoped></style>
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex: 10;
+        padding: 10px;
+        border: 1px solid #41b883;
+        outline: 0;
+    }
+    input[type="submit"] {
+        flex: 2;
+        background: #41b883;
+        color: #fff;
+        border: 1px solid #41b883;
+        cursor: pointer;
+    }
+</style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -21,10 +21,18 @@ const actions = {
     const res = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
     commit("setTodos", res.data);
   },
+  async addTodo({ commit }, title) {
+    const res = await axios.post(`https://jsonplaceholder.typicode.com/todos`, {
+      title,
+      complete: false,
+    });
+    commit("newTodo", res.data);
+  },
 };
 const mutations = {
   // The only way to actually change state in a Vuex store is by committing a mutation
   setTodos: (state, payload) => (state.todos = payload),
+  newTodo: (state, payload) => state.todos.unshift(payload),
 };
 
 export default {
